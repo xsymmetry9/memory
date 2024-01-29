cde986675d5fa46e4fa1ac3fcbaadd66b290856a
Gary <glswim@gmail.com>
2024-01-29 10:19:55 +0800

    Added an algo code for user to play a simple game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {v4 as uuid} from "uuid";
 import ReactDOM from 'react-dom';
+import IntroBox from './components/IntroBox';
 // import Timer from './components/Timer.js';
 // import Button from './components/Button.js';
 
@@ -25,21 +26,46 @@ async function createPokemon(id){
         clicked: false,
     };
 }
+const checkLevel = (item) => {
+    if(item === "easy")
+    {
+        return 3;
+    }
+    else if(item ==="medium")
+    {
+        return 5;
+    }
+    else if(item === "hard")
+    {
+        return 7;
+    } else{
+        return alert("error");
+    }
+}
 
 
 function App () {
     
     const Game = () =>{
-        const [numberOfCards, setNumberOfCards] = useState(10);
-        const [cards, setCards] = useState(Array.from({length: numberOfCards}, () => ({})));
-        const [score, setScore] = useState(0);
+        //Creates a player
+        const [introDisplay, setIntroDisplay] = useState(true);
+        const [player, setPlayer] = useState({name: "Gary", level: "hard", score: 0})
+
+        //game
+        const [isGameOver, isSetGameOver] = useState(false);
+        const [cards, setCards] = useState(Array.from({length: checkLevel(player.level)}, () => ({})));
         const [memory, setMemory] = useState([]);
+
+
+        //high score box
+        const [highScorer, setHighScorer] = useState({name:"", score: 0}); //could be set to local memory
+
 
         useEffect(() => {
             const fetchPokemon = async () => {
               try {
                 const newCards = await Promise.all(
-                  Array.from({ length: numberOfCards }, (_, index) => createPokemon(index + 1))
+                  Array.from({ length: checkLevel(player.level) }, (_, index) => createPokemon(index + 1))
                 );
                 
                 setCards(newCards);
@@ -49,50 +75,110 @@ function App () {
             };
       
             fetchPokemon();
-          }, [numberOfCards]);
+          }, [checkLevel(player.level)]);
      
 
-        
+        const clearMemory = () =>{
+            setMemory(prevMemory => 
+                {
+                    while(prevMemory.length > 0){
+                        prevMemory.pop();
+                    }
+                    return prevMemory;
+                });
+        }
+        const reset = () =>{
+            setPlayer({...player, score: 0})
+            clearMemory();
+            setCards(shuffledCards(cards));
+            isSetGameOver(false);
+        }
         const getInfo = (e) =>{
+            const pokemonName = e.currentTarget.name;
 
-            if(memory.includes(e.currentTarget.name))
+            if(memory.includes(pokemonName))
             {
-                alert("you lose");
-                setScore(0);
-                setCards(shuffledCards(cards));
+                console.log("you lose");
+                checksHighScorer();
+                isSetGameOver(true);
+                // reset();
             } else {
+                console.log("Phew...!")
                 memory.push(e.currentTarget.name);
-                setScore(prevScore => prevScore + 1);
-                setCards(shuffledCards(cards));
+                setPlayer({...player, score: player.score + 1});
+                if(memory.length === cards.length)
+                {
+                    console.log("You win!")
+                    checksHighScorer();
+                    isSetGameOver(true);
+             
+                } else {
+                    setCards(shuffledCards(cards));
+                }
             }
 
-        }
-        const handleInputCards = (e) =>{
-            setNumberOfCards(e.currentTarget.value);
+        };
+
+        const createNewPlayer = (name, level) =>{
+            setPlayer({...player, "name": name, "level": level});
         }
 
+        const checksHighScorer = () =>{
+            if(player.score > highScorer.score){
+                console.log(player.name + player.score);
+                setHighScorer({...highScorer, name: player.name, score: player.score})
+            }
+        }
+        const handleMenu = () =>{
+            reset();
+            setIntroDisplay(prevState => !prevState);
+        }
 
         return(
             <>
-                <h1>Memory Game</h1>
-                <label>Number of pokemon
-                    <input onChange={handleInputCards} value = {numberOfCards} />
-                </label>
-                <p>Score: {score}</p>
-                <div className="display-cards">
-                    {cards.map((item, index) =>{
-                        return(
-                        <button key ={index} className="cards" onClick={getInfo} name={item.name}>
-                            <img key={item.id} className="pokemon-card" src = {item.imgUrl} />
-                            <p className="pokemon-name" key={`${item.id}-text`}>{item.name}</p>
-                        </button>)
-                    })}
-           
-                </div>
-          
-     
-         
-                {/* {!gameOver ? <Timer setGameOver = {setGameOver} /> : <Button setGameOver = {setGameOver}/>} */}
+                <header>
+                    <div className="highScore-container">
+                            <p>Name: {highScorer.name}</p>
+                            <p>HighScore: {highScorer.score}</p>
+                    </div>
+                </header>
+                {introDisplay && <IntroBox player = {player} handlePlayer ={createNewPlayer} handle ={setIntroDisplay}/>}
+                {!introDisplay && 
+                
+                <div className="game-display">
+                    <h1 className="title">Memory Game</h1>
+
+                    {!isGameOver && 
+                        <>
+                            <div className="display-cards">
+                                {cards.map((item, index) =>{
+                                    return(
+                                    <button key ={index} className="cards" onClick={getInfo} name={item.name}>
+                                        <img key={item.id} className="pokemon-card" src = {item.imgUrl} />
+                                        <p className="pokemon-name" key={`${item.id}-text`}>{item.name}</p>
+                                    </button>)
+                                })}
+
+                                </div>
+                                <div className="score-board">
+                                <p className="left">{player.name}</p>
+                                <p className="right player-score">Score: {player.score}</p>
+                                </div>           
+                        </>            
+                    }
+                    {isGameOver && 
+                        <div>
+                            <h1>{player.name}, your score is {player.score}</h1>
+                            <p>Would you like to try again?</p>
+                            <button onClick={reset}>Enter</button>
+                            <button onClick= {handleMenu}>New Player</button>
+                        
+                        </div>}
+            
+
+                    </div>
+                
+                }
             </>
         )
     }
