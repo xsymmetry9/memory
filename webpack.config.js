f4ee9ab1e455024b00816a546bd62ab05ec67147
Gary <glswim@gmail.com>
2024-01-30 08:54:12 +0800

    Added one-line-code devtool

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: path.join(__dirname, "src", "index.js"),
+    devtool: 'inline-source-map',
     output:{
     path: path.resolve(__dirname, "dist"),
     },
