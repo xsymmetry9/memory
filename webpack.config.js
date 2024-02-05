3bc329c7bb515d4bf4e6c2762f64bbd08a3cba0d
Gary <glswim@gmail.com>
2024-02-05 11:17:26 +0800

    Enable to pack images

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,14 @@ module.exports = {
                     }
                 }
             },
-            
-            {test: /\.css$/i,
-            use: ["style-loader", "css-loader"],}
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        }
         ]
     },
     plugins: [
