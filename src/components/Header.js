8d6b73ef98795e45862198c8a4a5bf16fd6d80de
Gary <glswim@gmail.com>
2024-02-08 22:40:40 +0800

    Erased high score box from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,54 @@
 import React from "react";
 import logoURL from "./images/pokeball.png"
+import darkImg from "./images/night.png"
+import lightImg from "./images/light.png"
 
-const Header = ({theme, highScorer, darkWhiteBtn}) =>{
+const Header = ({theme, highScorer, darkWhiteBtn, isGameOver}) =>{
     const setBackground = () =>{
         return theme === "light" ? "light-theme" : "dark-theme";
     }
+    console.log(theme);
 
-    const HighScoreBox = (data) =>{
-        return(
-            <div className={`highScore-container ${setBackground}`}>
-                <p>Name: {data.name}</p>
-                <p>HighScore: {data.score}</p>
-            </div>
-        )
+    const darkLightBtnImg = () =>{
+        if(theme === "dark")
+        {
+            return darkImg;
+        } else {
+            return lightImg;
+        }
     }
+    // const HighScoreBox = (data) =>{
+    //     return(
+    //         <div className={`highScore-container ${setBackground} ${isGameOver ? "hidden" : ""}`}>
+    //             <p>Name: {data.name}</p>
+    //             <p>HighScore: {data.score}</p>
+    //         </div>
+    //     )
+    // }
     const DarkAndWhiteBtn = () =>{
         return(
-            <div className="darkWhiteToggleBtn">
-                <button name="dark" onClick={darkWhiteBtn}>{theme === "dark" ? "Dark" : "Light"}</button>
+            <div className="darkWhiteToggle">
+                <button 
+                className= {`darkWhiteToggleBtn ${theme === "light" ? "light" : "dark"}`}
+                name="dark" 
+                onClick={darkWhiteBtn}>
+                    <img src= {darkLightBtnImg()} alt="Dark/Light Button"/>
+                </button>
             </div>
         )
     }
     return(
         <header className= {`${theme === "light" ? "light-theme" : "dark-theme"}`}>
-            <div className="left">
-                <a href="https://www.flaticon.com/free-icons/pokemon" title="pokemon icons"><img className= "logo" src={logoURL}/></a>
-                <h1>Po-kery</h1>
+            <div className="header-container">
+                <div className="header-left">
+                    <a href="https://www.flaticon.com/free-icons/pokemon" title="pokemon icons"><img className= "logo" src={logoURL}/></a>
+                    <h1>Po-kery</h1>
+                </div>
+                <div className="right">
+                    <DarkAndWhiteBtn />
+                </div>
             </div>
-            <HighScoreBox data = {highScorer} />
-            <DarkAndWhiteBtn />
+       
         </header>
     )
 
