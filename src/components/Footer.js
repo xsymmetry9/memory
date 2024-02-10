256d9d0bd93e3513fc08757fd34826b10fd74f95
Gary <glswim@gmail.com>
2024-02-10 21:59:00 +0800

    Changed the scoreboard to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,39 @@ const now = new Date();
 const year = now.getFullYear();
 
 
-const Footer = ({theme}) =>{
+const Footer = ({theme, player}) =>{
+    const user = {"name": "xsymmetry", "project": "Odin Project", "year": year}
+    const FooterUser = () =>{
+        return(
+            <ul key="footer">
+                <li>© {user.year}</li>
+                <li>{user.name}</li>
+                <li>An {user.project}</li>
+            </ul>
+        )
+    }
+
+    const ScoreBoard = ({pName, pScore}) =>{
+        return (
+            
+            <div className="score-board">
+                <p className="left">{pName}</p>
+                <p className="right player-score">Score: {pScore}</p>
+            </div>  
+            
+        )
+    }
     return(
         <>
             <footer className={`${theme === "light" ? "light-theme" : "dark-theme"}`}>
                 <div className="container">
-                    <ul>
-                        <li>© {year}</li>
-                        <li>Xsymmetry</li>
-                        <li>An Odin Project</li>
-                    </ul>
-                
+                    {player.isGameOver && <FooterUser/>}
+                    {!(player.isGameOver) && <ScoreBoard pName = {player.name} pScore ={player.score}/>}
                 </div>
             </footer>
+            
+
+
         </>
     )
 }
